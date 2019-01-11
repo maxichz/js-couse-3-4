01ffd78a517e9acd59cb41797cff318e5b2a9d51
dev-tim <artem.zadorozhniy@gmail.com>
2019-01-11 21:13:11 +0100

    Js functions lesson

--- 01-intro-js/index.js
+++ 01-intro-js/index.js
@@ -1,1 +1,41 @@
-console.log('Hello world')
+function fizzBuzz(n) {
+   console.log("n", n);
+
+   if (n % 3 === 0 && n % 2 === 0){
+        console.log("fizzbizz")
+   } else if (n % 3 === 0) {
+        console.log("bizz")
+    } else if (n % 2 === 0) {
+        console.log("/n")
+    } else {
+        console.log("fuzz")
+    }
+}
+
+function foo (boo, number){
+    boo(number);
+}
+
+function add (a, b){
+    return a + b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+function multiply(a,b){
+    return a * b;
+}
+
+var operationsDB = {
+    "*" : multiply,
+    "+" : add,
+    "/" : divide
+}
+
+function calculator(opreration, a, b){
+    return operationsDB[opreration](a, b);
+}
+
+console.log(calculator("*", 897, 8713));
