c6de900fd45b0ffe0e3d2ec4309c5961bbe82264
dev-tim <artem.zadorozhniy@gmail.com>
2019-01-17 21:08:36 +0100

    Added setup for homework 2

--- 02-count-buttons/scripts/app.js
+++ 02-count-buttons/scripts/app.js
@@ -1,4 +1,12 @@
 (function(){
+    var state = {
+        first: 0,
+        second: 0
+    }
+
+    function updateLabel(labelElement, prefix, count){
+        labelElement.innerText = prefix + ' ' + count;
+    }
 
     var labelFirst = document.querySelector('#label-first');
     var firstButton = document.querySelector('#btn-first');
@@ -7,10 +15,12 @@
     var secondButton = document.querySelector('#btn-second');
 
     firstButton.addEventListener('click', function(e){
-        alert('first')
+        state.first = state.first + 1;
+        updateLabel(labelFirst, 'First button:', state.first)
     })
 
     secondButton.addEventListener('click', function(e){
-        alert('second')
+        state.second = state.second + 1;
+        updateLabel(labelSecond, 'Second button:', state.second)
     });
 })()
